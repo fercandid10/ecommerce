5bbc2bf5d967a18f47bf24449da541808c1fb9d2
theflash1409 <theflash1409@gmail.com>
2021-08-10 21:41:28 -0300

    Criando a classe PageAdmin e TplAdmin

--- index.php
+++ index.php
@@ -4,11 +4,21 @@ require_once("vendor/autoload.php");
 
 $app = new \Slim\Slim();
 
+use \Hcode\PageAdmin;
+
 $app->config('debug', true);
 
 $app->get('/', function() {
     
 	$page = new Hcode\Page();
+
+	$page->setTpl("index");
+
+});
+
+$app->get('/admin', function() {
+    
+	$page = new PageAdmin();
 
 	$page->setTpl("index");
 
